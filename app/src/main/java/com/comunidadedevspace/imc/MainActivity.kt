71102ade6f5b9c784f45d638ee37acd11677a33e
Roque Buarque <roqueb@spotify.com>
2023-10-30 21:12:58 +0100

    refactor: Criando InputLayout e Recuperando dados da UI

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,34 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Recuperar os valores digitados pelo usuário na EditText
+        //Criar variavel e associar o componente de UI<textInputEditText>
+
+        val edtPeso: TextInputEditText = findViewById(R.id.edit_peso)
+        val edtAltura: TextInputEditText = findViewById(R.id.edit_altura)
+
+        val btnCalcular: Button = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso= edtPeso.text.toString()
+            val altura= edtAltura.text.toString()
+
+            calcularImc(peso.toDouble(), altura.toDouble())
+        }
+
+    }
+
+
+    fun calcularImc(peso: Double, altura: Double): Double {
+        return peso / (altura * altura)
     }
 }
